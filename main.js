728ec3a50f41b69a9bf4335ba8488158bef5dbc4
Leevi Mertanen <leevi.mertanen@itvelhot.local>
2020-09-09 09:09:13 +0300

    redi

--- main.js
+++
@@ -1,7 +0,0 @@
-var cookiecount = 0;
-
-function add() {
-    cookiecount = cookiecount + 1
-    document.getElementById('text').value = cookiecount;
-    document.title = cookiecount + "Cookies";
-}
